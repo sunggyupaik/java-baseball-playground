4df4c83344b23e70c28f11af135562085a14fa69
sunggyupaik <melon9751@naver.com>
2023-09-13 20:50:46 +0900

    [refactor] 테스트의 공통 코드를 상속으로 대체하라
    연산 시 사용하는 숫자값이 중복되므로 별도의 클래스로 분리하여
    각 클래스에서 상속해 사용하도록 개선합니다
    
    모든 테스트에서 숫자값을 정의할 필요가 없어 중복이 제거됩니다

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -6,70 +6,72 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class Calculator {
+	abstract static class ContextTwoNumber {
+		double givenNumber1() {
+			return 10d;
+		}
+
+		double givenNumber2() {
+			return 5d;
+		}
+	}
+
 	@Nested
 	@DisplayName("calculate 메서드는")
 	class Describe_calculate {
 		@Nested
 		@DisplayName("만약 2개의 숫자와 덧셈 연산자가 주어진다면")
-		class Context_with_plusOperation_num1_num2 {
+		class Context_with_plusOperation_num1_num2 extends ContextTwoNumber {
 			private final String operation = "+";
-			private final double num1 = 5d;
-			private final double num2 = 10d;
 
-			private final double result = Operation.calculate(operation, num1, num2);
+			private final double result = Operation.calculate(operation, givenNumber1(), givenNumber2());
 
 			@Test
 			@DisplayName("덧셈 결과를 리턴한다")
 			void it_returns_a_valid_plus_result() {
-				Assertions.assertEquals(result, num1 + num2);
+				Assertions.assertEquals(result, givenNumber1() + givenNumber2());
 			}
 		}
 
 		@Nested
 		@DisplayName("만약 2개의 숫자와 뺄셈 연산자가 주어진다면")
-		class Context_with_minusOperation_num1_num2 {
+		class Context_with_minusOperation_num1_num2 extends ContextTwoNumber {
 			private final String operation = "-";
-			private final double num1 = 10d;
-			private final double num2 = 5d;
 
-			private final double result = Operation.calculate(operation, num1, num2);
+			private final double result = Operation.calculate(operation, givenNumber1(), givenNumber2());
 
 			@Test
 			@DisplayName("뺄셈 결과를 리턴한다")
 			void it_returns_a_valid_minus_result() {
-				Assertions.assertEquals(result, num1 - num2);
+				Assertions.assertEquals(result, givenNumber1() - givenNumber2());
 			}
 		}
 
 		@Nested
 		@DisplayName("만약 2개의 숫자와 곱셈 연산자가 주어진다면")
-		class Context_with_timesOperation_num1_num2 {
+		class Context_with_timesOperation_num1_num2 extends ContextTwoNumber {
 			private final String operation = "*";
-			private final double num1 = 10d;
-			private final double num2 = 5d;
 
-			private final double result = Operation.calculate(operation, num1, num2);
+			private final double result = Operation.calculate(operation, givenNumber1(), givenNumber2());
 
 			@Test
 			@DisplayName("곱셈 결과를 리턴한다")
 			void it_returns_a_valid_times_result() {
-				Assertions.assertEquals(result, num1 * num2);
+				Assertions.assertEquals(result, givenNumber1() * givenNumber2());
 			}
 		}
 
 		@Nested
 		@DisplayName("만약 2개의 숫자와 나눗셈 연산자가 주어진다면")
-		class Context_with_divideOperation_num1_num2 {
+		class Context_with_divideOperation_num1_num2 extends ContextTwoNumber {
 			private final String operation = "/";
-			private final double num1 = 10d;
-			private final double num2 = 5d;
 
-			private final double result = Operation.calculate(operation, num1, num2);
+			private final double result = Operation.calculate(operation, givenNumber1(), givenNumber2());
 
 			@Test
 			@DisplayName("곱셈 결과를 리턴한다")
 			void it_returns_a_valid_times_result() {
-				Assertions.assertEquals(result, num1 / num2);
+				Assertions.assertEquals(result, givenNumber1() / givenNumber2());
 			}
 		}
 	}
